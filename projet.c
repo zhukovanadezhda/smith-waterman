3f6a8786cc1a5fd73ff705bc78547dc16897becd
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-03-18 21:12:52 +0300

    initial commit

--- projet.c
+++ projet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+ 
 #define NOM1_PAR_DEFAUT "seq1.txt"
 #define NOM2_PAR_DEFAUT "seq2.txt"
 
-
+ 
 /*
  * Function:  read_sequence 
  * ----------------------------------------
